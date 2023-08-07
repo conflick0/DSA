221950c1d7aa22a9f489953df532eb4d809153d0
conflick0 <jackhsu0099@gmail.com>
2023-08-07 12:01:54 +0800

    update sort

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -22,7 +22,8 @@ if __name__ == '__main__':
     inp = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
     gt = [23, 23, 29, 34, 55, 66, 67, 78, 78, 79, 88, 89, 92, 96, 96, 100]
 
-    out = sort(inp)
-    test(out, gt)
     print('inp:', inp)
+    out = sort(inp)
+
     print('out:', out)
+    test(out, gt)
